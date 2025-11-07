bd1891c5f34253ff49224bffb1b97f310dcc7eb5
Vis-halV <vishal06076@gmail.com>
2025-11-07 21:48:21 +0530

    feat: added icsees_2026-brochure

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,15 +1,33 @@
+import Brochure from '../assets/ICSEES-2026_Brochure.pdf';
+
 function Home() {
   return (
     <section id="home" className="home-section">
       <div className="inner-home">
+        
+        {/* Top Marquee */}
         <div className="marquee-container">
           <div className="marquee-content">
-              <a href="./images/step 2021 new.pdf">ICSEES-2026 BROCHURE</a>&nbsp;&nbsp;&nbsp;
-              <a href="#jp">Journal Publication <sup id="text-red">New</sup></a>&nbsp;&nbsp;&nbsp;
-              <a href="STEP 2021_Inaugural Invitation and Agenda.pdf">Inaugural Function Invitation</a>&nbsp;&nbsp;&nbsp;
+            <a href={Brochure} target="_blank" rel="noopener noreferrer">
+              ICSEES-2026 BROCHURE
+            </a>
+            &nbsp;&nbsp;&nbsp;
+            <a href="#jp">
+              Journal Publication <sup id="text-red">New</sup>
+            </a>
+            &nbsp;&nbsp;&nbsp;
+            <a
+              href="STEP_2021_Inaugural_Invitation_and_Agenda.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Inaugural Function Invitation
+            </a>
+            &nbsp;&nbsp;&nbsp;
           </div>
         </div>
 
+        {/* Main Content */}
         <div className="home-content">
           <h3>DEPARTMENT OF INFORMATION TECHNOLOGY</h3>
           <h3>DEPARTMENT OF ELECTRICAL & ELECTRONICS ENGINEERING</h3>
@@ -20,17 +38,47 @@ function Home() {
           <div className="home-buttons">
             <a href="#overview" className="btn">LEARN MORE</a>
             <a href="#contact" className="btn btn-danger">REGISTER NOW</a>
-            <a href="https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fwww.svce.ac.in%2Fstep2021%2Fimages%2Fpaper-format.docx&wdOrigin=BROWSELINK" target="_blank" className="btn">PAPER FORMAT</a>
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfc4UXQvTxYntEW3cEbtbVZxTa4sKLmE5d1tPxA68MbtmqI0A/viewform?usp=header" target="_blank" className="btn">Register & Payment Proof</a>
+            <a
+              href="https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fwww.svce.ac.in%2Fstep2021%2Fimages%2Fpaper-format.docx&wdOrigin=BROWSELINK"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn"
+            >
+              PAPER FORMAT
+            </a>
+            <a
+              href="https://docs.google.com/forms/d/e/1FAIpQLSfc4UXQvTxYntEW3cEbtbVZxTa4sKLmE5d1tPxA68MbtmqI0A/viewform?usp=header"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn"
+            >
+              Register & Payment Proof
+            </a>
           </div>
         </div>
 
+        {/* Bottom Marquee */}
         <div className="marquee-container">
           <div className="marquee-content bottom-marquee">
-              <a href="final Program Schedule.pdf">Programme Schedule</a>&nbsp;&nbsp;&nbsp;
-              <a href="STEP 2021_Valedictory Invitation and Agenda.pdf">Valedictory Function Invitation</a>&nbsp;&nbsp;&nbsp;
+            <a
+              href="final_Program_Schedule.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Programme Schedule
+            </a>
+            &nbsp;&nbsp;&nbsp;
+            <a
+              href="STEP_2021_Valedictory_Invitation_and_Agenda.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Valedictory Function Invitation
+            </a>
+            &nbsp;&nbsp;&nbsp;
           </div>
         </div>
+
       </div>
     </section>
   );
